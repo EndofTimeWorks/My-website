d62572aac9525ee45818f24961382fc908b67d39
End Nightshade <contact@endoftimee.tech>
2026-02-16 22:40:09 -0700

    fix: resolve eslint errors and remove dead code

--- src/reportWebVitals.tsx
+++ src/reportWebVitals.tsx
@@ -1,18 +1,20 @@
-﻿const reportWebVitals = (onPerfEntry?: (metric: any) => void): void => {
-    if (onPerfEntry && typeof onPerfEntry === "function") {
-        import("web-vitals")
-            .then((vitals) => {
-                const { onCLS, onFID, onFCP, onLCP, onTTFB } = vitals;
-                onCLS(onPerfEntry);
-                onFID(onPerfEntry);
-                onFCP(onPerfEntry);
-                onLCP(onPerfEntry);
-                onTTFB(onPerfEntry);
-            })
-            .catch((error) => {
-                console.error("Error loading web-vitals:", error);
-            });
-    }
+﻿import type { Metric } from "web-vitals";
+
+const reportWebVitals = (onPerfEntry?: (metric: Metric) => void): void => {
+  if (onPerfEntry && typeof onPerfEntry === "function") {
+    import("web-vitals")
+      .then((vitals) => {
+        const { onCLS, onFID, onFCP, onLCP, onTTFB } = vitals;
+        onCLS(onPerfEntry);
+        onFID(onPerfEntry);
+        onFCP(onPerfEntry);
+        onLCP(onPerfEntry);
+        onTTFB(onPerfEntry);
+      })
+      .catch((error) => {
+        console.error("Error loading web-vitals:", error);
+      });
+  }
 };
 
 export default reportWebVitals;
